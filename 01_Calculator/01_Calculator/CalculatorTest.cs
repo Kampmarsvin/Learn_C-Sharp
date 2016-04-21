6bb856f2e17113c0b945cf84b647e857bd04b88c
Kampmarsvin <simon@stochholm.net>
2016-04-21 13:15:03 +0200

    Added calculator

---
+++ 01_Calculator/01_Calculator/CalculatorTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _01_Calculator
+{
+    [TestClass]
+    public class CalculatorTest
+    {
+        [TestMethod]
+        public void Add()
+        {
+            Assert.AreEqual(0, Calculator.Add(0, 0));
+            Assert.AreEqual(4, Calculator.Add(2, 2));
+            Assert.AreEqual(8, Calculator.Add(2, 6));
+        }
+        [TestMethod]
+        public void Subtract()
+        {
+            Assert.AreEqual(6, Calculator.Subtract(10, 4));
+        }
+        [TestMethod]
+        public void Sum()
+        {
+            //Arrange
+            int[] numbers = new int[] { };
+            Assert.AreEqual(0, Calculator.Sum(numbers));
+            
+            numbers[0] = 7;
+            Assert.AreEqual(7, Calculator.Sum(numbers));
+            
+            numbers[0] = 7;
+            numbers[1] = 11;
+            Assert.AreEqual(18, Calculator.Sum(numbers));
+
+
+
+        }
+        
+    }
+}
